7a7dc1962b64ba3b4dca0990474826b573d2a1c4
DorivalJunior23 <dorivaljunior23@outlook.com>
2022-10-31 21:02:14 -0300

    melhorando o http

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,29 +24,44 @@ app.get('/produtos', async (request, response) =>{
             }))
  })
 
-// esse get nao ta como many to many tem que relacionar com pedido produto
-// app.get('/pedido/:id/produtos', async (request, response)=>{
-//     const pedidoId = request.params.id
-//     const produtos = await prisma.produto.findMany({
-//        select:{
-//         id: true,
-//         nome: true,
-//         descricao: true,
-//         valor: true,
-//         fotoUrl: true,
-//        },
-//        where:{
-//         pedidoId, 
-//        },
-//        orderBy:{
-//         valor: 'desc'
-//        }
-//     })
-//     return response.json(produtos.map(produto => {
-//         return{
-//             ...produto,
-//             valor: converterIntEmReais(produto.valor)
-//         }
-//     }))
-// })
+// esse get nao ta como many to many tem que relacionar com ProdutoPedido
+app.get('/pedidos/:id/produtos', async (request, response)=>{
+    const pedidoId = request.params.id
+    const produtoPedido = await prisma.produtoPedido.findMany({
+        select:{
+        produtoId: true,
+        },
+       where:{
+        pedidoId, 
+       },
+    })
+    return response.json(produtoPedido.map(produtoPedido => {
+        return{
+            ...produtoPedido,
+        }
+    }))
+})
+
+app.get('/produtos/:id', async (request, response) =>{
+    const id = request.params.id
+    const produto = await prisma.produto.findUnique({
+        where:{
+            id,
+        }
+    })
+    return response.json(produto)
+ })
+
+ app.post('/pedidos/:id/produtos', async (request,response)=>{ 
+    const pedidoId = request.params.id;
+    const body: any = request.body;
+  
+    const produtoPedido = await prisma.produtoPedido.create({
+      data: {
+        pedidoId,
+        produtoId: body.produtoId,
+      }
+    })
+    return response.json(produtoPedido);
+  });
 app.listen(3333);
